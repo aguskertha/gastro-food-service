07c033dc6bb25611e8046c6987c761371f7d93cd
aguskertha <baguskertha@gmail.com>
2022-09-12 16:10:49 +0700

    feat(config): configuration project with database

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
 var express = require('express');
+const bodyParser = require('body-parser');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
+const cors = require('cors');
 var app = express();
+require('dotenv').config();
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({ limit: '50mb', extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/public',express.static(path.join(__dirname, 'public')));
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
+app.get('/', (req, res, next) => {
+    res.send("<h1>Hello</h1>")
+});
 module.exports = app;
