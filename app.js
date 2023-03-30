38ac3a84293dca30f0a8b3fc3826237414df4a19
aguskertha <baguskertha@gmail.com>
2023-03-30 19:22:52 +0700

    feat(predict): update max body parser

--- app.js
+++ app.js
@@ -32,8 +32,6 @@ app.set('view engine', 'ejs');
 app.use(expressLayouts);
 app.use(cors());
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 app.use(session({
   secret: 'secret',
